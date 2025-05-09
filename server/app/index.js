b5109dc84186692a2047353e440b22231241575c
edurelated2021 <183998296+edurelated2021@users.noreply.github.com>
2025-05-09 16:17:51 +0530

    Updates

--- server/app/index.js
+++ server/app/index.js
@@ -9,10 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./routes/chatAgent')); 
-app.use(require('./routes/imageAnalyzerAgent')); 
-app.use(require('./routes/outdoorAdvisorAgent'));
-app.use(require('./routes/productRecommendationsAgent'));
+app.use(require('../../routes/chatAgent')); 
+app.use(require('../../routes/imageAnalyzerAgent')); 
+app.use(require('../../routes/outdoorAdvisorAgent'));
+app.use(require('../../routes/productRecommendationsAgent'));
+
+// Define a route
+app.get('/hello', (req, res) => {
+  res.send('Hello');
+});
 
 // Serve the HTML file
 app.use(express.static(path.join(__dirname, '/')));
