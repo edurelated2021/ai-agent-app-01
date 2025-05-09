571e45298acf2d38f5f61e093e6f9998ecd08383
edurelated2021 <183998296+edurelated2021@users.noreply.github.com>
2025-05-09 22:46:55 +0530

    a

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+app.use(express.json());
+app.options('*', cors()); // Respond to preflight
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
